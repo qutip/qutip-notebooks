551826ed7330a273891b3faf4214cceaf0c34b9b
christian512 <c.staufe@posteo.net>
2022-04-05 21:24:40 +0200

    formatting

--- tests/test_notebooks_for_errors.py
+++ tests/test_notebooks_for_errors.py
@@ -1,7 +1,6 @@
 import nbformat
-import os
 from nbconvert.preprocessors import ExecutePreprocessor
-import pytest
+
 
 def get_notebook_errors(path):
     """ Executes a notebook and returns occured errors """
@@ -28,14 +27,13 @@ def test_piqs_superradiance():
     errors = get_notebook_errors('examples/piqs_superradiance.ipynb')
     assert not errors
 
+
 def test_piqs_entropy_purity():
     errors = get_notebook_errors('examples/piqs-entropy_purity.ipynb')
     assert not errors
 
+
 def test_measure_trajectories_cat_kerrs():
-    errors = get_notebook_errors('examples/measures-trajectories-cats-kerr.ipynb')
+    errors = get_notebook_errors(
+        'examples/measures-trajectories-cats-kerr.ipynb')
     assert not errors
-
-
-
-
